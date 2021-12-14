da56e301ef0a03c1c42d139dfd118ab2b66cbab2
Bruno Portugal <bportugal90@gmail.com>
2021-12-14 12:51:22 -0300

    First commit - Spring Added

---
+++ src/test/java/com/marsrovers/MarsRoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsrovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
