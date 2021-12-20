e1588abd8ba48ec020f4a8ecdcf5a45e63f4d6c8
Bruno Portugal <bportugal90@gmail.com>
2021-12-20 08:46:50 -0300

    Removed some commented lines

--- src/main/java/com/marsrovers/rovers/movement/Directions.java
+++ src/main/java/com/marsrovers/rovers/movement/Directions.java
@@ -106,44 +106,4 @@ public enum Directions {
 
     public abstract Map<String, Integer> nextPosition(Map<String, Integer> position, Surface surface);
 
-
-    /*public static String rotate90(String command) {
-        switch (direction) {
-            case "N":
-                return Directions.N.nextDirection(direction);
-            case "E":
-                return Directions.E.nextDirection(direction);
-            case "W":
-                return Directions.W.nextDirection(direction);
-            case "S":
-                return Directions.S.nextDirection(direction);
-            default:
-                return "Direction not valid";
-        }
-    }*/
-
-    /*public static Map<String, Integer> nextPosition(Integer x, Integer y, String direction, Surface surface) {
-        Map<String, Integer> position = new HashMap<>();
-        switch (direction) {
-            case "N":
-                position.put("X", x);
-                position.put("Y", Directions.N.move(y, surface));
-                return position;
-            case "E":
-                position.put("X", Directions.E.move(x, surface));
-                position.put("Y", y);
-                return position;
-            case "W":
-                position.put("X", Directions.W.move(x, surface));
-                position.put("Y", y);
-                return position;
-            case "S":
-                position.put("X", x);
-                position.put("Y", Directions.S.move(y, surface));
-                return position;
-            default:
-                return null;
-        }
-    }*/
-
 }
